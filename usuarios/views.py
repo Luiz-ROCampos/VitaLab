b652ca9432ec83a706de00e5227587c81a86c7e4
Luiz Ricardo <ricardoxluiz88@gmail.com>
2023-10-13 06:52:24 -0300

    Foi adicionado login.html e feito a função logar

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 def cadastro(request):
@@ -44,3 +45,22 @@ def cadastro(request):
 
     
     return redirect('/usuarios/cadastro')
+
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+        
+        if user:
+            login(request, user)
+            redirect('/')
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuário ou senha não estão corretos.')
+            return redirect('/usuarios/login')
+        
+        return HttpResponse(f'{username} - {senha}')
+
