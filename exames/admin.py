f2d724045f4cbda295f1e1d7a8ce6df7c2a60ff8
Luiz Ricardo <ricardoxluiz88@gmail.com>
2023-10-14 10:52:29 -0300

    Criação das models do app exame: as tabelas TipoExames, SolicitaçãoExame, PedidosExames.

--- exames/admin.py
+++ exames/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TipoExames, SolicitacaoExame, PedidosExames
 
-# Register your models here.
+admin.site.register(TipoExames)
+admin.site.register(SolicitacaoExame)
+admin.site.register(PedidosExames)
